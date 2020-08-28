53b762e034bd20ada39d83389a0d01c0b4086a18
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-08-28 10:20:21 +0200

    Separate install directories for each architectuer

--- configureCommand.sh
+++ configureCommand.sh
@@ -1,19 +1,26 @@
 #! /bin/sh
 
 # Change prefix to avoid issues with Lib / lib
+PREFIX=$PWD
+XCFRAMEWORKS_DIR=$PREFIX/../Python-aux/
+export PATH=$PREFIX/install_macosx/bin:$PATH
+OSX_SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+IOS_SDKROOT=$(xcrun --sdk iphoneos --show-sdk-path)
+SIM_SDKROOT=$(xcrun --sdk iphonesimulator --show-sdk-path)
+
 # 1) compile for OSX (required)
 
-env CC=clang CXX=clang++ CPPFLAGS="-isysroot /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/" CFLAGS="-isysroot /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/" CXXFLAGS="-isysroot /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/" LDFLAGS="-isysroot /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/" LDSHARED="clang -v -undefined error -dynamiclib -isysroot /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/ -lz -L. -lpython3.9" ./configure --prefix=/Users/holzschu/src/Xcode_iPad/cpython/install_macosx --with-system-ffi --enable-shared
-make
-make install
+env CC=clang CXX=clang++ CPPFLAGS="-isysroot /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/" CFLAGS="-isysroot /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/" CXXFLAGS="-isysroot /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/" LDFLAGS="-isysroot /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/" LDSHARED="clang -v -undefined error -dynamiclib -isysroot /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/ -lz -L. -lpython3.9" ./configure --prefix=$PREFIX/install_macosx --with-system-ffi --enable-shared
+make >& make_osx.log
+make install >& make_install_osx.log
 
 # 2) compile for iOS:
 
 # 2.1) download and install required packages: 
-curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libffi.xcframework.zip
+# to do later, after several cycles of debug.
+# curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libffi.xcframework.zip
 
 # preadv / pwritev are iOS 14+ only
-export PATH=/Users/holzschu/src/Xcode_iPad/cpython/bin:$PATH
 env CC=clang CXX=clang++ \
 	CPPFLAGS="-arch arm64 -miphoneos-version-min=14.0 -isysroot /Applications/Xcode-beta.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/" \
 	CFLAGS="-arch arm64 -miphoneos-version-min=14.0 -isysroot /Applications/Xcode-beta.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/" \
@@ -21,9 +28,9 @@ env CC=clang CXX=clang++ \
 	LDFLAGS="-arch arm64 -miphoneos-version-min=14.0 -isysroot/Applications/Xcode-beta.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/ -F/Users/holzschu/src/Xcode_iPad/cpython/Frameworks_iphoneos -framework ios_system -L/Users/holzschu/src/Xcode_iPad/cpython/Frameworks_iphoneos/lib" \
 	LDSHARED="clang -v -undefined error -dynamiclib -isysroot /Applications/Xcode-beta.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/ -lz -L. -lpython3.9  -F/Users/holzschu/src/Xcode_iPad/cpython/Frameworks_iphoneos -framework ios_system -L/Users/holzschu/src/Xcode_iPad/cpython/Frameworks_iphoneos" \
 	PLATFORM=iphoneos \
-	./configure --prefix=/Users/holzschu/src/Xcode_iPad/cpython/install_iphoneos --enable-shared \
+	./configure --prefix=$PREFIX/install_iphoneos --enable-shared \
 	--host arm-apple-darwin --build x86_64-apple-darwin --enable-ipv6 \
-	--with-openssl=/Users/holzschu/src/Xcode_iPad/cpython/Frameworks_iphoneos \
+	--with-openssl=$PREFIX/Frameworks_iphoneos \
 	with_system_ffi=yes \
 	ac_cv_file__dev_ptmx=no \
 	ac_cv_file__dev_ptc=no \
@@ -31,9 +38,9 @@ env CC=clang CXX=clang++ \
 	ac_cv_func_sendfile=no \
 	ac_cv_func_clock_settime=no
 
-cp -r ../Python-aux/libffi.xcframework/ios-arm64/Headers/* Frameworks_iphoneos/include/ffi/
-cp -r ../Python-aux/crypto.xcframework/ios-arm64/Headers/* Frameworks_iphoneos/include/crypto/
-cp -r ../Python-aux/openssl.xcframework/ios-arm64/Headers/* Frameworks_iphoneos/include/openssl/
+cp -r ../Python-aux/libffi.xcframework/ios-arm64/Headers/* $PREFIX/Frameworks_iphoneos/include/ffi/
+cp -r ../Python-aux/crypto.xcframework/ios-arm64/Headers/* $PREFIX/Frameworks_iphoneos/include/crypto/
+cp -r ../Python-aux/openssl.xcframework/ios-arm64/Headers/* $PREFIX/Frameworks_iphoneos/include/openssl/
 # Need to copy all libs after each make clean: 
 cp ../Python-aux/crypto.xcframework/ios-arm64/libcrypto.a Frameworks_iphoneos/lib/
 cp ../Python-aux/openssl.xcframework/ios-arm64/libssl.a Frameworks_iphoneos/lib/
@@ -41,7 +48,7 @@ cp ../Python-aux/libffi.xcframework/ios-arm64/libffi.a Frameworks_iphoneos/lib/
 find . -name \*.o -delete
 make
 
-
+# Now create frameworks from dynamic libraries & incorporate changes into code.
 
 # Python build finished successfully!
 # The necessary bits to build these optional modules were not found:
