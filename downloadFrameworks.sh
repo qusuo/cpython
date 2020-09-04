a7b557ddb00786d43abeadbdd7d328fbabb773b3
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-09-04 14:51:16 +0200

    Added downloading of ios_system

--- downloadFrameworks.sh
+++ downloadFrameworks.sh
@@ -5,10 +5,12 @@ pushd Python-aux
 curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libffi.xcframework.zip
 curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/crypto.xcframework.zip
 curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/openssl.xcframework.zip
+curl -OL https://github.com/holzschu/ios_system/releases/download/2.6/ios_system.xcframework.zip
 
-rm -rf libffi.xcframework crypto.xcframework openssl.xcframework
+rm -rf libffi.xcframework crypto.xcframework openssl.xcframework ios_system.xcframework
 unzip -q libffi.xcframework.zip
 unzip -q crypto.xcframework.zip
 unzip -q openssl.xcframework.zip
+unzip -q ios_system.xcframework.zip
 
 popd
