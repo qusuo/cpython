5fd0ffb9f5be916471623844d251ab998f189970
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-09-04 14:06:13 +0200

    new script to compile from scratch

---
+++ downloadAndCompile.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+downloadFrameworks.sh
+buildAllArchitectures.sh
+createFrameworks.sh
+createModuleFrameworks.sh
+
+# If we upload all packages to a repository, uncomment and create Package.swift 
+# createPackage_swift.sh
+
