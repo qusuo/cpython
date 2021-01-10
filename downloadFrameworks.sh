6883cb6b264b23f5d75406eef9405cf3b4d22bd7
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2021-01-10 15:49:03 +0100

    Edited for libgfortran support

--- downloadFrameworks.sh
+++ downloadFrameworks.sh
@@ -6,6 +6,7 @@ pushd Python-aux
 curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libffi.xcframework.zip
 curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/crypto.xcframework.zip
 curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/openssl.xcframework.zip
+curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/openblas.xcframework.zip
 curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libzmq.xcframework.zip
 curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libjpeg.xcframework.zip
 curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libtiff.xcframework.zip
@@ -16,10 +17,11 @@ curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libxslt.xc
 curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libexslt.xcframework.zip
 curl -OL https://github.com/holzschu/ios_system/releases/download/2.6/ios_system.xcframework.zip
 
-rm -rf libffi.xcframework crypto.xcframework openssl.xcframework ios_system.xcframework libzmq.xcframework libjpeg.xcframework libtiff.xcframework freetype.xcframework harfbuzz.xcframework libpng.xcframework libxslt.xcframework libexslt.xcframework
+rm -rf libffi.xcframework crypto.xcframework openssl.xcframework openblas.xcframework ios_system.xcframework libzmq.xcframework libjpeg.xcframework libtiff.xcframework freetype.xcframework harfbuzz.xcframework libpng.xcframework libxslt.xcframework libexslt.xcframework
 unzip -q libffi.xcframework.zip
 unzip -q crypto.xcframework.zip
 unzip -q openssl.xcframework.zip
+unzip -q openblas.xcframework.zip
 unzip -q libzmq.xcframework.zip
 unzip -q libjpeg.xcframework.zip
 unzip -q libtiff.xcframework.zip
@@ -31,3 +33,16 @@ unzip -q libexslt.xcframework.zip
 unzip -q ios_system.xcframework.zip
 
 popd
+
+# Set to 1 if you have gfortran for arm64 installed. gfortran support is highly experimental.
+# You might need to edit the script as well.
+USE_FORTRAN=0
+if [ -e "/usr/local/aarch64-apple-darwin20/lib/libgfortran.dylib" ];then
+        USE_FORTRAN=1
+fi
+if [$USE_FORTRAN == 1];
+then
+	# Create libgfortran library:
+	LIBGCC_BUILD=/Users/holzschu/src/Xcode_iPad/gcc-build/aarch64-apple-darwin20/libgcc
+	clang -miphoneos-version-min=11.0 -arch arm64 -dynamiclib -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.3.sdk -install_name @rpath/libgfortran.framework/libgfortran -o Frameworks_iphoneos/lib/libgfortran.dylib /usr/local/aarch64-apple-darwin20/lib/libgfortran.a -all_load $LIBGCC_BUILD/cas_8_4.o $LIBGCC_BUILD/ldadd_4_4.o $LIBGCC_BUILD/swp_4_2.o $LIBGCC_BUILD/ldadd_4_1.o $LIBGCC_BUILD/lse-init.o -compatibility_version 6.0.0 
+fi
