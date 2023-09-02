28d69b5d81568379c892a1711cb1e3d6038bc967
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2023-09-02 09:16:12 +0200

    Updated list of packgages

--- downloadFrameworks.sh
+++ downloadFrameworks.sh
@@ -3,37 +3,36 @@
 curl -OL https://github.com/holzschu/ios_system/releases/download/v3.0.1/ios_error.h
 mkdir -p Python-aux
 pushd Python-aux
-curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libffi.xcframework.zip
-curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/crypto.xcframework.zip
-curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/openssl.xcframework.zip
-curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/openblas.xcframework.zip
-curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libzmq.xcframework.zip
-curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libjpeg.xcframework.zip
-curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libtiff.xcframework.zip
-curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/freetype.xcframework.zip
-curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/harfbuzz.xcframework.zip
-curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libpng.xcframework.zip
-curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libxslt.xcframework.zip
-curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libexslt.xcframework.zip
-curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libfftw3.xcframework.zip
-curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/libfftw3_threads.xcframework.zip
-curl -OL https://github.com/holzschu/ios_system/releases/download/v3.0.1/ios_system.xcframework.zip
 
-rm -rf libffi.xcframework crypto.xcframework openssl.xcframework openblas.xcframework ios_system.xcframework libzmq.xcframework libjpeg.xcframework libtiff.xcframework freetype.xcframework harfbuzz.xcframework libpng.xcframework libxslt.xcframework libexslt.xcframework libfftw3.xcframework libfftw3_threads.xcframework
-unzip -q libffi.xcframework.zip
-unzip -q crypto.xcframework.zip
-unzip -q openssl.xcframework.zip
-unzip -q openblas.xcframework.zip
-unzip -q libzmq.xcframework.zip
-unzip -q libjpeg.xcframework.zip
-unzip -q libtiff.xcframework.zip
-unzip -q freetype.xcframework.zip
-unzip -q harfbuzz.xcframework.zip
-unzip -q libpng.xcframework.zip
-unzip -q libxslt.xcframework.zip
-unzip -q libexslt.xcframework.zip
-unzip -q libfftw3.xcframework.zip
-unzip -q libfftw3_threads.xcframework.zip
+for library in 
+libpng \
+libffi \
+libzmq \
+openblas \
+freetype \
+harfbuzz \
+crypto \
+openssl \
+libjpeg \
+libtiff \
+libxslt \
+libexslt \
+libfftw3 \
+libfftw3_threads \
+libspatialindex_c \
+libspatialindex \
+libgdal \
+libproj \
+libgeos_c \
+libgeos \
+liblzma
+do 
+	curl -OL https://github.com/holzschu/Python-aux/releases/download/1.0/$library.xcframework.zip
+	rm -rf $library.xcframework 
+	unzip -q $library.xcframework.zip
+done
+
+curl -OL https://github.com/holzschu/ios_system/releases/download/v3.0.1/ios_system.xcframework.zip
 unzip -q ios_system.xcframework.zip
 
 popd
